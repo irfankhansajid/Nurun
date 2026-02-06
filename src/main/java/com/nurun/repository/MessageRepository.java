185a4e023bdd0caadda1ef37837ec79a34f7b662
irfankhansajid <irfankhansajid@gmail.com>
2026-02-07 01:52:25 +0600

    feat(Repository): implement jpaRepository with optional and query method

---
+++ src/main/java/com/nurun/repository/MessageRepository.java
@@ -0,0 +1,14 @@
+package com.nurun.repository;
+
+import com.nurun.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    List<Message> findAllByConversationIdOrderBySentAtAsc(Long id);
+
+}
